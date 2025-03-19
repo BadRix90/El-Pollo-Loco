006a3ee10298db76f2d5538b0e4cbacdbda18dbb
Kay <118884667+BadRix90@users.noreply.github.com>
2025-03-19 20:59:43 +0100

    add animation

--- models/cloud.class.js
+++ models/cloud.class.js
@@ -1,17 +1,16 @@
-class Cloud extends MovableObject{
-    y = 70;
-    height = 300;
-    width =720;
+class Cloud extends MovableObject {
+  y = 70;
+  height = 300;
+  width = 720;
 
-
-    constructor(){
-        super().loadImage('img/5_background/layers/4_clouds/full.png')
-        this.x = 0 + Math.random() * 500;
-
-    }
-
-
-
-
-
+  constructor() {
+    super().loadImage("img/5_background/layers/4_clouds/full.png");
+    this.x = 0
+    setInterval(() => {
+      this.x -= 0.2;
+      if (this.x < -this.width) {
+        this.x = canvas.width;
+      }
+    }, 1000 / 60);
+  }
 }
