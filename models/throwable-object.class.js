37561b50cfc47ac2b437c43f324de44d57f77c1e
Kay <118884667+BadRix90@users.noreply.github.com>
2025-03-27 00:00:47 +0100

    ♻️ Simplify bullet behavior to fly until collision or offscreen

--- models/throwable-object.class.js
+++ models/throwable-object.class.js
@@ -1,12 +1,11 @@
 class Bullet extends MovableObject {
-  constructor(x, y, speed = 7, owner = null) {
+  constructor(x, y, speed = 7) {
     super().loadImage('img/cyberpunk-characters-pixel-art/guns/5 Bullets/1.png');
     this.x = x;
     this.y = y;
     this.width = 20;
     this.height = 10;
     this.speed = speed;
-    this.owner = owner;
     this.animate();
   }
 
