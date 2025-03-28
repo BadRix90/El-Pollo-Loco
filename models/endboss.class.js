05db0f2fd0a35b028c21ed12c92dd0795bfdc5b9
Kay <118884667+BadRix90@users.noreply.github.com>
2025-03-28 12:07:35 +0100

    feat: Add random AI decision for Endboss to choose between standard and special attack

--- models/endboss.class.js
+++ models/endboss.class.js
@@ -29,7 +29,6 @@ class Endboss extends MovableObject {
     "img/cyberpunk-characters-pixel-art/10_boss/Boss_three/frames/Attack3/Attack3_frame_6.png",
   ];
 
-
   IMAGES_HURT = [
     "img/cyberpunk-characters-pixel-art/10_boss/Boss_three/frames/Hurt/Hurt_frame_1.png",
     "img/cyberpunk-characters-pixel-art/10_boss/Boss_three/frames/Hurt/Hurt_frame_2.png",
@@ -57,36 +56,44 @@ class Endboss extends MovableObject {
   startAI() {
     setInterval(() => {
       if (!this.world || this.isDead() || this.isAttacking) return;
-  
+
       const distance = Math.abs(this.world.character.x - this.x);
       const attackRange = 250;
-  
+
       if (distance < attackRange) {
-        this.startAttack();
+        const r = Math.random();
+        if (
+          r < 0.3 &&
+          (!this.lastSpecial || Date.now() - this.lastSpecial > 10000)
+        ) {
+          this.startSpecialAttack();
+          this.lastSpecial = Date.now(); // Cooldown für Spezialangriff
+        } else {
+          this.startAttack();
+        }
       }
     }, 300);
   }
-  
 
   startAttack() {
     if (this.isDead() || this.isAttacking) return;
-  
+
     this.isAttacking = true;
     this.mode = "attack";
     this.currentImage = 0;
-  
+
     let i = 0;
     let interval = setInterval(() => {
       this.img = this.imageCache[this.IMAGES_ATTACK[i]];
       i++;
-  
+
       if (i === this.IMAGES_ATTACK.length - 1) {
         const direction = this.world.character.x < this.x ? -1 : 1;
         const bulletX = this.x + (direction === 1 ? this.width - 40 : 30);
         const bulletY = this.y + this.height - 100;
         this.world.spawnBullet(bulletX, bulletY, direction, this, 1);
       }
-  
+
       if (i >= this.IMAGES_ATTACK.length) {
         clearInterval(interval);
         this.isAttacking = false;
@@ -94,14 +101,14 @@ class Endboss extends MovableObject {
       }
     }, 100);
   }
-  
+
   startSpecialAttack() {
     if (this.isDead() || this.isAttacking) return;
-  
+
     this.isAttacking = true;
     this.mode = "special";
     this.currentImage = 0;
-  
+
     let i = 0;
     let specialInterval = setInterval(() => {
       this.img = this.imageCache[this.IMAGES_ATTACK_SPECIAL[i]];
@@ -116,7 +123,7 @@ class Endboss extends MovableObject {
   spawnBomb() {
     const bomb = new Bomb(this.world.character.x, -200, this.world);
     this.world.activeBombs.push(bomb);
-  
+
     setTimeout(() => {
       this.isAttacking = false;
       this.mode = "idle";
@@ -154,11 +161,9 @@ class Endboss extends MovableObject {
 
   animate() {
     setInterval(() => {
-      if (this.isDead() || this.deathPlayed || this.isAttacking) return; 
+      if (this.isDead() || this.deathPlayed || this.isAttacking) return;
       this.playAnimation(this.IMAGES_WALKING);
       this.otherDirection = true;
     }, 200);
   }
-  
-  
 }
