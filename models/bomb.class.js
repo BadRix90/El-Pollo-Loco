7569c01e47d6f9eff8fc68ac2969952d50e5944a
Kay <118884667+BadRix90@users.noreply.github.com>
2025-03-28 12:05:54 +0100

    fix: Correct animation frame limits for Bomb and add optional explosion damage to player

--- models/bomb.class.js
+++ models/bomb.class.js
@@ -1,80 +1,89 @@
 class Bomb extends MovableObject {
-    constructor(x, y, world) {
-      super().loadImage("img/cyberpunk-characters-pixel-art/10_boss/Boss_three/frames/Bomb/Bomb_frame_1.png");
-      this.loadImages(this.IMAGES_ATTACK_SPECIAL_BOMB_FLY);
-      this.loadImages(this.IMAGES_ATTACK_SPECIAL_BOMB);
-      this.loadImages(this.IMAGES_ATTACK_SPECIAL_BOMB_BOOM);
-      this.loadImages(this.IMAGES_ATTACK_SPECIAL_BOMB_BOOM_DUST);
-      this.x = x;
-      this.y = y;
-      this.world = world;
-      this.speedY = -20;
-      this.gravity = 2;
-      this.state = "fly";
-      this.animate();
-    }
-  
-    IMAGES_ATTACK_SPECIAL_BOMB_FLY = [
-        "img/cyberpunk-characters-pixel-art/10_boss/Boss_three/frames/Bomb/Bomb_frame_1.png",
-      ];
-    
-      IMAGES_ATTACK_SPECIAL_BOMB = [
-        "img/cyberpunk-characters-pixel-art/10_boss/Boss_three/frames/Attack4/Attack4_frame_1.png",
-        "img/cyberpunk-characters-pixel-art/10_boss/Boss_three/frames/Attack4/Attack4_frame_2.png",
-        "img/cyberpunk-characters-pixel-art/10_boss/Boss_three/frames/Attack4/Attack4_frame_3.png",
-        "img/cyberpunk-characters-pixel-art/10_boss/Boss_three/frames/Attack4/Attack4_frame_4.png",
-        "img/cyberpunk-characters-pixel-art/10_boss/Boss_three/frames/Attack4/Attack4_frame_5.png",
-        "img/cyberpunk-characters-pixel-art/10_boss/Boss_three/frames/Attack4/Attack4_frame_6.png",
-      ];
-    
-      IMAGES_ATTACK_SPECIAL_BOMB_BOOM = [
-        "img/cyberpunk-characters-pixel-art/10_boss/Boss_three/frames/BOOM_bomb/BOOM_bomb_frame_1.png",
-        "img/cyberpunk-characters-pixel-art/10_boss/Boss_three/frames/BOOM_bomb/BOOM_bomb_frame_2.png",
-        "img/cyberpunk-characters-pixel-art/10_boss/Boss_three/frames/BOOM_bomb/BOOM_bomb_frame_3.png",
-        "img/cyberpunk-characters-pixel-art/10_boss/Boss_three/frames/BOOM_bomb/BOOM_bomb_frame_4.png",
-      ];
-    
-      IMAGES_ATTACK_SPECIAL_BOMB_BOOM_DUST = [
-        "img/cyberpunk-characters-pixel-art/10_boss/Boss_three/frames/Dust/Dust_frame_1.png",
-        "img/cyberpunk-characters-pixel-art/10_boss/Boss_three/frames/Dust/Dust_frame_2.png",
-        "img/cyberpunk-characters-pixel-art/10_boss/Boss_three/frames/Dust/Dust_frame_3.png",
-      ];
-  
-    animate() {
-      let i = 0;
-  
-      let interval = setInterval(() => {
-        if (this.state === "fly") {
-          this.y += 8;
-          this.playAnimation(this.IMAGES_ATTACK_SPECIAL_BOMB_FLY);
-          if (this.y >= this.defaultYPosition) {
-            this.y = this.defaultYPosition;
-            this.state = "idle";
-            i = 0;
-          }
-        } else if (this.state === "idle") {
-          this.playAnimation(this.IMAGES_ATTACK_SPECIAL_BOMB);
-          i++;
-          if (i > this.IMAGES_ATTACK_SPECIAL_BOMB.length) {
-            this.state = "boom";
-            i = 0;
-          }
-        } else if (this.state === "boom") {
-          this.playAnimation(this.IMAGES_ATTACK_SPECIAL_BOMB_BOOM);
-          i++;
-          if (i > this.IMAGES_ATTACK_SPECIAL_BOMB_BOOM.length) {
-            this.state = "dust";
-            i = 0;
-          }
-        } else if (this.state === "dust") {
-          this.playAnimation(this.IMAGES_ATTACK_SPECIAL_BOMB_BOOM_DUST);
-          i++;
-          if (i > this.IMAGES_ATTACK_SPECIAL_BOMB_BOOM_DUST.length) {
-            clearInterval(interval);
-            this.world.activeBombs = this.world.activeBombs.filter(b => b !== this);
-          }
-        }
-      }, 100);
-    }
+  constructor(x, y, world) {
+    super().loadImage(
+      "img/cyberpunk-characters-pixel-art/10_boss/Boss_three/frames/Bomb/Bomb_frame_1.png"
+    );
+    this.loadImages(this.IMAGES_ATTACK_SPECIAL_BOMB_FLY);
+    this.loadImages(this.IMAGES_ATTACK_SPECIAL_BOMB);
+    this.loadImages(this.IMAGES_ATTACK_SPECIAL_BOMB_BOOM);
+    this.loadImages(this.IMAGES_ATTACK_SPECIAL_BOMB_BOOM_DUST);
+    this.x = x;
+    this.y = y;
+    this.world = world;
+    this.speedY = -20;
+    this.gravity = 2;
+    this.state = "fly";
+    this.animate();
   }
-  
+
+  IMAGES_ATTACK_SPECIAL_BOMB_FLY = [
+    "img/cyberpunk-characters-pixel-art/10_boss/Boss_three/frames/Bomb/Bomb_frame_1.png",
+  ];
+
+  IMAGES_ATTACK_SPECIAL_BOMB = [
+    "img/cyberpunk-characters-pixel-art/10_boss/Boss_three/frames/Attack4/Attack4_frame_1.png",
+    "img/cyberpunk-characters-pixel-art/10_boss/Boss_three/frames/Attack4/Attack4_frame_2.png",
+    "img/cyberpunk-characters-pixel-art/10_boss/Boss_three/frames/Attack4/Attack4_frame_3.png",
+    "img/cyberpunk-characters-pixel-art/10_boss/Boss_three/frames/Attack4/Attack4_frame_4.png",
+    "img/cyberpunk-characters-pixel-art/10_boss/Boss_three/frames/Attack4/Attack4_frame_5.png",
+    "img/cyberpunk-characters-pixel-art/10_boss/Boss_three/frames/Attack4/Attack4_frame_6.png",
+  ];
+
+  IMAGES_ATTACK_SPECIAL_BOMB_BOOM = [
+    "img/cyberpunk-characters-pixel-art/10_boss/Boss_three/frames/BOOM_bomb/BOOM_bomb_frame_1.png",
+    "img/cyberpunk-characters-pixel-art/10_boss/Boss_three/frames/BOOM_bomb/BOOM_bomb_frame_2.png",
+    "img/cyberpunk-characters-pixel-art/10_boss/Boss_three/frames/BOOM_bomb/BOOM_bomb_frame_3.png",
+    "img/cyberpunk-characters-pixel-art/10_boss/Boss_three/frames/BOOM_bomb/BOOM_bomb_frame_4.png",
+  ];
+
+  IMAGES_ATTACK_SPECIAL_BOMB_BOOM_DUST = [
+    "img/cyberpunk-characters-pixel-art/10_boss/Boss_three/frames/Dust/Dust_frame_1.png",
+    "img/cyberpunk-characters-pixel-art/10_boss/Boss_three/frames/Dust/Dust_frame_2.png",
+    "img/cyberpunk-characters-pixel-art/10_boss/Boss_three/frames/Dust/Dust_frame_3.png",
+  ];
+
+  animate() {
+    let i = 0;
+
+    let interval = setInterval(() => {
+      if (this.state === "fly") {
+        this.y += 8;
+        this.playAnimation(this.IMAGES_ATTACK_SPECIAL_BOMB_FLY);
+        if (this.y >= this.defaultYPosition) {
+          this.y = this.defaultYPosition;
+          this.state = "idle";
+          i = 0;
+        }
+      } else if (this.state === "idle") {
+        this.playAnimation(this.IMAGES_ATTACK_SPECIAL_BOMB);
+        i++;
+        if (i >= this.IMAGES_ATTACK_SPECIAL_BOMB.length) {
+          this.state = "boom";
+          i = 0;
+        }
+      } else if (this.state === "boom") {
+        this.playAnimation(this.IMAGES_ATTACK_SPECIAL_BOMB_BOOM);
+        i++;
+
+        if (i === 1 && this.isColliding(this.world.character)) {
+          this.world.character.hit();
+          this.world.statusBar.setPercentage(this.world.character.energy);
+        }
+
+        if (i >= this.IMAGES_ATTACK_SPECIAL_BOMB_BOOM.length) {
+          this.state = "dust";
+          i = 0;
+        }
+      } else if (this.state === "dust") {
+        this.playAnimation(this.IMAGES_ATTACK_SPECIAL_BOMB_BOOM_DUST);
+        i++;
+        if (i >= this.IMAGES_ATTACK_SPECIAL_BOMB_BOOM_DUST.length) {
+          clearInterval(interval);
+          this.world.activeBombs = this.world.activeBombs.filter(
+            (b) => b !== this
+          );
+        }
+      }
+    }, 100);
+  }
+}
