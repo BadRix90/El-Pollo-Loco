a25f2fa49b1d48d279929edf7170b057710142f8
Kay <118884667+BadRix90@users.noreply.github.com>
2025-03-31 21:13:35 +0200

    feat: add subtle blinking effect to status bar on low health
    Implemented pulse-based globalAlpha in status bar draw method.
    Triggers gentle blinking animation when HP drops below 40%.

--- models/status-bar.class.js
+++ models/status-bar.class.js
@@ -1,16 +1,16 @@
 class StatusBar extends DrawableObject {
   IMAGES_FULL = [
-    'IMG/GUI/2 BARS/HEALTHBAR1.PNG',
-    'IMG/GUI/2 BARS/HEALTHBAR2.PNG',
-    'IMG/GUI/2 BARS/HEALTHBAR3.PNG',
-    'IMG/GUI/2 BARS/HEALTHBAR4.PNG'
+    "IMG/GUI/2 BARS/HEALTHBAR1.PNG",
+    "IMG/GUI/2 BARS/HEALTHBAR2.PNG",
+    "IMG/GUI/2 BARS/HEALTHBAR3.PNG",
+    "IMG/GUI/2 BARS/HEALTHBAR4.PNG",
   ];
 
   IMAGES_EMPTY = [
-    'IMG/GUI/2 BARS/HEALTHBAR5.PNG',
-    'IMG/GUI/2 BARS/HEALTHBAR6.PNG',
-    'IMG/GUI/2 BARS/HEALTHBAR7.PNG',
-    'IMG/GUI/2 BARS/HEALTHBAR8.PNG'
+    "IMG/GUI/2 BARS/HEALTHBAR5.PNG",
+    "IMG/GUI/2 BARS/HEALTHBAR6.PNG",
+    "IMG/GUI/2 BARS/HEALTHBAR7.PNG",
+    "IMG/GUI/2 BARS/HEALTHBAR8.PNG",
   ];
 
   percentage = 100;
@@ -28,18 +28,42 @@ class StatusBar extends DrawableObject {
   draw(ctx) {
     const partWidth = this.width / 4;
 
+    const hpPercent = this.percentage / 100;
+    let opacity = 1;
+
+    if (hpPercent < 0.5) {
+      const pulse = Math.sin(Date.now() / 200) * 0.1 + 0.9;
+      opacity = Math.min(1, pulse);
+    }
+
+    ctx.save();
+    ctx.globalAlpha = opacity;
+
     this.IMAGES_EMPTY.forEach((path, index) => {
       const img = this.imageCache[path];
-      ctx.drawImage(img, this.x + index * partWidth, this.y, partWidth, this.height);
+      ctx.drawImage(
+        img,
+        this.x + index * partWidth,
+        this.y,
+        partWidth,
+        this.height
+      );
     });
 
-
-    const hpParts = Math.ceil((this.percentage / 100) * 4);
+    const hpParts = Math.ceil(hpPercent * 4);
     for (let i = 0; i < hpParts; i++) {
       const path = this.IMAGES_FULL[i];
       const img = this.imageCache[path];
-      ctx.drawImage(img, this.x + i * partWidth, this.y, partWidth, this.height);
+      ctx.drawImage(
+        img,
+        this.x + i * partWidth,
+        this.y,
+        partWidth,
+        this.height
+      );
     }
+
+    ctx.restore();
   }
 
   setPercentage(percentage) {
