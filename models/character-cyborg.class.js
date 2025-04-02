d518ab8948f407c465b1c27401afa2781a833f88
BadRix90 <118884667+BadRix90@users.noreply.github.com>
2025-04-02 20:32:44 +0200

    Update character-cyborg.class.js

---
+++ models/character-cyborg.class.js
@@ -0,0 +1,8 @@
+class CharacterCyborg extends Character {
+    constructor() {
+      super();
+      this.loadImage('img/cyberpunk-characters-pixel-art/3 Cyborg/frames/Cyborg_idle_frame_1.png');
+      this.loadImages(this.IMAGES_IDLE);
+    }
+  }
+  
