0aad251973ad3656a6ec269752b31b0c445cdf77
BadRix90 <118884667+BadRix90@users.noreply.github.com>
2025-04-02 20:32:27 +0200

    feat: Add CharacterCyborg and CharacterPunk classes

---
+++ models/character-punk.class.js
@@ -0,0 +1,9 @@
+class CharacterPunk extends Character {
+    constructor() {
+      super();
+      this.loadImage('img/punk/frames/Punk_idle_frame_1.png');
+      this.loadImages(this.IMAGES_IDLE);
+     
+    }
+  }
+  
